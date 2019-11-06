4dbcd47d71a96d9f67b9f67e5d9c86cb80be82e3
Karen V <karenvo@usc.edu>
2019-11-06 09:14:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,18 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      
+    end
 
+    get '/new' do
+      erb :root
+    end
+
+    get '/pirates' do
+
+    end
+
+    
   end
 end
